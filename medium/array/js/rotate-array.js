49e0c0f5640ccb8561fdfd2d99dfd5328d3ecb53
LAKSHMIKANT <lakshmikant4u@gmail.com>
2022-12-16 00:05:41 +0530

    Array : Javascript solution to rotate array by k steps

---
+++ medium/array/js/rotate-array.js
@@ -0,0 +1,39 @@
+/** https://leetcode.com/problems/rotate-array/
+ 
+Rotate Array
+
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Test Case 1
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Test Case 2
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+
+Constraints:
+
+1 <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+0 <= k <= 105
+
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+let nums = [1, 2, 3, 4, 5, 6, 7], k = 3;
+const rotate1 = (nums, k) => nums.unshift(...nums.splice(-k % nums.length));
+rotate1(nums, k)
+console.log(nums)
+
