c5dcafdca3ca13fd35c72b1ea4853c4dbfe2be53
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-01-08 01:28:43 +0530

    Solution 2 Max sub array

--- medium/array/js/maximum-subarray.js
+++ medium/array/js/maximum-subarray.js
@@ -38,3 +38,22 @@ const maxSubArray = (nums) => {
 
 console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])) // 6
 
+// Solution 2
+
+const maxSubArray2 = (nums) => {
+    let max = nums[0];
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        if (sum > max) {
+            max = sum;
+        }
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+    return max;
+};
+
+console.log(maxSubArray2([5, 4, -1, 7, 8])) // 23
+
