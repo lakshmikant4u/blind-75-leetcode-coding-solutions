7892e854aeb762c1fc9bd9840dfb5aab3c3fbf9d
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-01-07 07:21:45 +0530

    JS Solution for max prod subarray

---
+++ medium/array/js/maximum-product-subarray.js
@@ -0,0 +1,44 @@
+/** https://leetcode.com/problems/maximum-product-subarray/
+
+Maximum Product Subarray
+
+Given an integer array nums, find a subarray that has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+Test Case 1
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+
+Test Case 2
+Input: nums = [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-10 <= nums[i] <= 10
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+*/
+
+// Solution 1
+
+const maxProduct = (nums) => {
+    let mini, maxi, res;
+    mini = maxi = res = nums[0];
+
+    for (let num of nums.slice(1)) {
+        const currMini = Math.min(num, num * maxi, num * mini);
+        const currMaxi = Math.max(num, num * maxi, num * mini);
+        maxi = currMaxi
+        mini = currMini
+        res = Math.max(res, maxi)
+    }
+
+    return res
+};
+
+console.log(maxProduct([2, 3, -2, 4])) // 6
